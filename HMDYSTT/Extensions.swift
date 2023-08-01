d7a44b1d8d05dce5ad60aee77b7acc3ace42a70a
amirkaidarov <KaidarovAmir1441@gmail.com>
2023-08-01 15:51:27 -0400

    Added Color scheme

---
+++ HMDYSTT/Extensions.swift
@@ -0,0 +1,15 @@
+//
+//  Extensions.swift
+//  HMDYSTT
+//
+//  Created by Амир Кайдаров on 8/1/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let background = Color("Background")
+    static let icon = Color("Icon")
+    static let text = Color("Text")
+    static let systemBackground = Color(uiColor: .systemBackground)
+}
---
+++ HMDYSTT/TransactionModel.swift
@@ -0,0 +1,29 @@
+//
+//  TransactionModel.swift
+//  HMDYSTT
+//
+//  Created by Амир Кайдаров on 8/1/23.
+//
+
+import Foundation
+
+struct TransactionModel : Identifiable {
+    let id: Int
+    let date: String
+    let institution: String
+    let account: String
+    var merchant: String
+    let amount: Double
+    let type: TransactionType.RawValue
+    var categoryId: Int
+    var category: String
+    let isPending: Bool
+    var isTransfer: Bool
+    var isExpense: Bool
+    var isEdited: Bool
+}
+
+enum TransactionType: String {
+    case debit = "debit"
+    case credit = "credit"
+}
