38c383b2b9920ea9a73588f0cdcf9e5e2bfab8e1
amirkaidarov <KaidarovAmir1441@gmail.com>
2023-08-01 15:15:53 -0400

    Initial Commit

---
+++ HMDYSTT/HMDYSTTApp.swift
@@ -0,0 +1,17 @@
+//
+//  HMDYSTTApp.swift
+//  HMDYSTT
+//
+//  Created by Амир Кайдаров on 8/1/23.
+//
+
+import SwiftUI
+
+@main
+struct HMDYSTTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
